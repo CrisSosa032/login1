9bf7aab107149fcaed083b85dba0ddcf26361678
Cristian Sosa <bossun032@gmail.com>
2024-08-19 22:17:02 -0300

    segundo comit creacion del front

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    getUsers();
+  }, []);
+  
+  const getUsers = async() => {
+    try {
+      const response = await fetch("http://http://localhost:3001/users")
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  return (
+    <>
+    <h1>Hola mundo</h1>
+    </>
+  )
+}
+
+export default App
