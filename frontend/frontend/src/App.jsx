103a82405e5fc3fdfdb1e5cbf62a2a82082c5103
Cristian Sosa <bossun032@gmail.com>
2024-08-23 16:13:17 -0300

    mostramos los datos de la db en el front

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -3,23 +3,38 @@ import './App.css'
 
 
 function App() {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     getUsers();
   }, []);
   
   const getUsers = async() => {
     try {
-      const response = await fetch("http://http://localhost:3001/users")
-      console.log(response);
+      const response = await fetch("http://localhost:3001/users")
+      const data = await response.json();
+      setUsers(data.data)
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  console.log (users)
 
 
   return (
     <>
     <h1>Hola mundo</h1>
+    {
+      users.map(user => {
+        return(
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+
+        </div>
+        )
+      })
+    }
     </>
   )
 }
